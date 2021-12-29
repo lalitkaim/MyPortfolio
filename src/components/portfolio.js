b5c6baf91fbbe9e8f41bc3716f424cec35701f93
Lalit <lalit@intel.com>
2021-12-29 21:10:10 +0530

    add first component

---
+++ src/components/portfolio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class portfolio extends Component {
+    render() {
+        return (
+            <div>
+                portfolio
+            </div>
+        );
+    }
+}
+
+export default portfolio;
